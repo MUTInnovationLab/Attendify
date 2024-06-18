f53a44d94adc5abf4542b2568ec2005727b79082
Jabulani00 <jabulanimosesgwala@gmail.com>
2024-06-18 13:01:27 +0200

    gf

---
+++ src/app/stude-scan/stude-scan.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stude-scan',
+  templateUrl: './stude-scan.page.html',
+  styleUrls: ['./stude-scan.page.scss'],
+})
+export class StudeScanPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
