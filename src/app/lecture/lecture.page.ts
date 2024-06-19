d80c034d6c03389aef8196c9ea01dfc3ddd79b9c
Jabulani00 <jabulanimosesgwala@gmail.com>
2024-06-19 10:17:29 +0200

    f

--- src/app/lecture/lecture.page.ts
+++ src/app/lecture/lecture.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController, ToastController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app'; // Import firebase app
 
 @Component({
   selector: 'app-lecture',
@@ -6,10 +11,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecture.page.scss'],
 })
 export class LecturePage implements OnInit {
+  showAddCard: boolean = false;
+  
+  contact_nom: string = '';
+  contact_email: string = '';
+  contact_sujet: string = '';
+  contact_message: string = '';
 
-  constructor() { }
+  moduleName: any;
+  moduleCode: any;
+  moduleLevel: any;
+  userData: any;
+  tableData: any[] = [];
+
+  constructor(private router: Router,
+    private navCtrl: NavController,
+    private loadingController: LoadingController,
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private alertController: AlertController,
+    private toastController: ToastController) { }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  async addModule() {
+    const loader = await this.loadingController.create({
+      message: 'submitting...',
+      cssClass: 'custom-loader-class',
+    });
+    await loader.present();
+
+    try {
+      // Get the currently logged-in user
+      const user = firebase.auth().currentUser;
+
+      if (user) {
+        // If user is logged in, add the module to a new collection named 'modules'
+        // and include the user's email in the module document
+        await this.db.collection('modules').add({
+          moduleName: this.moduleName,
+          moduleCode: this.moduleCode,
+          moduleLevel: this.moduleLevel,
+          userEmail: user.email,
+        });
+
+        // Clear the input fields
+        this.moduleName = '';
+        this.moduleCode = '';
+        this.moduleLevel = '';
+
+        loader.dismiss();
+        alert('Module successfully saved');
+      } else {
+        // Handle case where user is not logged in
+        loader.dismiss();
+        alert('User not logged in.');
+      }
+    } catch (error) {
+      loader.dismiss();
+      console.error('Error saving module:', error);
+      alert('An error occurred while saving the module.');
+    }
+  }
+
+  gotoQRscan(moduleCode: string) {
+    this.router.navigate(['qr-scan'], { queryParams: { moduleCode } });
+  }
+
+  gotoProfile(moduleCode: string) {
+    this.router.navigate(['profile']);
+  }
+
+  gotoAttendies(moduleCode: string) {
+    this.router.navigate(['attendies']);
+  }
+
+  getData() {
+    // Fetch modules associated with the logged-in user's email
+    const user = firebase.auth().currentUser;
+
+    if (user) {
+      this.db
+        .collection('modules', ref => ref.where('userEmail', '==', user.email))
+        .snapshotChanges()
+        .subscribe((data) => {
+          this.userData = data.map((d) => {
+            const id = d.payload.doc.id;
+            const docData = d.payload.doc.data() as any; // Cast docData as any type
+            return { id, ...docData };
+          });
+          console.log(this.userData);
+          this.tableData = this.userData;
+        });
+    } else {
+      console.log('User not logged in.');
+    }
   }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,118 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+  emailRegex: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  adminEmail: string = 'mutinnovationlab@gmail.com';
+  adminPassword: string = 'InnovationLab123';
 
-  ngOnInit() {
+  constructor(
+    private db: AngularFirestore,
+    private loadingController: LoadingController,
+    private auth: AngularFireAuth,
+    private navController: NavController,
+    private toastController: ToastController
+  ) {}
+
+  ngOnInit() {}
+
+  async validate() {
+    if (!this.email || !this.password) {
+      this.presentToast('Please enter your email and password.');
+      return;
+    }
+    if (!this.emailRegex.test(this.email)) {
+      this.presentToast('Please provide a valid email address.');
+      return;
+    }
+    await this.login();
   }
 
+  async login() {
+    const loader = await this.loadingController.create({
+      message: 'Signing in',
+      cssClass: 'custom-loader-class'
+    });
+  
+    try {
+      await loader.present();
+  
+      if (this.email === 'mutinnovationlab@gmail.com' && this.password === 'InnovationLab123') {
+        loader.dismiss();
+        this.navController.navigateForward('/admin');
+        return;
+      }
+  
+      const userCredential = await this.auth.signInWithEmailAndPassword(this.email, this.password);
+  
+      if (userCredential) {
+        const userType = await this.getUserType();
+        loader.dismiss();
+        this.navigateBasedOnUserType(userType);
+      }
+    } catch (error) {
+      loader.dismiss();
+      const errorMessage = (error as Error).message;
+  
+      switch (errorMessage) {
+        case 'Firebase: The password is invalid or the user does not have a password. (auth/wrong-password)':
+        case 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found)':
+          this.presentToast('Invalid email or password');
+          break;
+        case 'Firebase: The email address is badly formatted. (auth/invalid-email)':
+          this.presentToast('Incorrectly formatted email');
+          break;
+        default:
+          this.presentToast('An unexpected error occurred.');
+          break;
+      }
+    }
+  }
+  
+
+  checkAdminCredentials(): boolean {
+    return this.email === this.adminEmail && this.password === this.adminPassword;
+  }
+
+  async getUserType(): Promise<string> {
+    const studentQuerySnapshot = await this.db.collection('registeredStudents')
+      .ref.where('email', '==', this.email)
+      .get();
+    if (!studentQuerySnapshot.empty) {
+      return 'student';
+    }
+
+    const staffQuerySnapshot = await this.db.collection('registered staff')
+      .ref.where('email', '==', this.email).where('staffNumber', '==', this.password)
+      .get();
+    if (!staffQuerySnapshot.empty) {
+      return 'staff';
+    }
+
+    return 'unknown';
+  }
+
+  navigateBasedOnUserType(userType: string) {
+    switch (userType) {
+      case 'student':
+        this.navController.navigateForward('/stude-scan');
+        break;
+      case 'staff':
+        this.navController.navigateForward('/lecture');
+        break;
+      default:
+        alert('User not found in registeredStudents or registeredStaff collections.');
+        break;
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: 'danger'
+    });
+    toast.present();
+  }
 }
