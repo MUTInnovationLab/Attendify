97529e1c0422990c49e8879f0cb77205284a94ce
unknown <Admin@MUTR1606.mut.ac.za>
2024-07-23 10:05:16 +0200

    changes

--- src/app/services/qr-scanner.services.ts
+++ src/app/services/qr-scanner.services.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-import { AngularFireDatabase } from '@angular/fire/database';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class QrScannerService {
-  constructor(private barcodeScanner: BarcodeScanner, private db: AngularFireDatabase) {}
+  constructor() {}
 
-  scanQrCode() {
-    return this.barcodeScanner.scan();
-  }
 
-  storeScannedData(data: any) {
-    const filteredData = this.filterData(data);
-    return this.db.list('scanned-data').push(filteredData);
-  }
+
+
 
   private filterData(data: any) {
     // Perform any data filtering or transformation here
